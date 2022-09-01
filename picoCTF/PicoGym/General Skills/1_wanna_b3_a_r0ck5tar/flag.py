d0d69fe637f218c6640fef64f85f30219f066e4f
Myricht <kennethvanmele@hotmail.com>
2022-09-01 19:03:05 +0200

    Added
    asm4
    1_wanna_b3_a_r0ck5tar
    b00tl3gRSA2

---
+++ picoCTF/PicoGym/General Skills/1_wanna_b3_a_r0ck5tar/flag.py
@@ -0,0 +1,28 @@
+# Rocknroll = True
+# Silence = False
+# a_guitar = 10
+# Tommy = 44
+# Music = 170
+# the_music = input()
+# if the_music == a_guitar:
+#     print("Keep on rocking!")
+#     the_rhythm = input()
+#     if the_rhythm - Music == False:
+Tommy = 66
+print(chr(Tommy))
+Music = 79
+Jamming = 78
+print(chr(Music))
+print(chr(Jamming))
+Tommy = 74
+print(chr(Tommy))
+# They are dazzled audiences
+print(chr(Music))
+Rock = 86
+print(chr(Rock))
+Tommy = 73
+print(chr(Tommy))
+#     break
+#     print("Bring on the rock!")
+# else: print("That ain't it, Chief")
+#     break
